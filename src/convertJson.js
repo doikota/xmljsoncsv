7277d2e48410e1f80f755019bf1b1df9a7771763
doikota <doikota@gmail.com>
2024-09-20 00:13:25 +0900

    Accepted Array (partial)

--- src/convertJson.js
+++ src/convertJson.js
@@ -2,39 +2,43 @@ import { Parser } from 'json2csv';
 
 function convertJson() {
   // Convert JSON to CSV
-  // const jsonData = document.getElementById('jsonInput').value;
-  const jsonData = [
-    {
-        "title": "吾輩は猫である",
-        "author": "夏目 漱石",
-        "year": 1905,
-        "genre": "小説",
-        "chapter": {
-          "title": "第一章", "page": 1
-        }
-    },
-    {
-        "title": "ノルウェイの森",
-        "author": "村上 春樹",
-        "year": 1987,
-        "genre": "小説",
-        "chapter": {
-          "title": "第一部", "page": 1
-        }
-    }
-  ]
-  
+  const jsonText = document.getElementById('jsonInput').value;
+  const jsonData = JSON.parse(jsonText);
   console.log(jsonData);
+  const fieldArrays = {};
+
+  jsonData.forEach(item => {
+    Object.keys(item).forEach(key => {
+        if (typeof item[key] === 'object' && !Array.isArray(item[key])) {
+            Object.keys(item[key]).forEach(subKey => {
+                fieldArrays[`${key}.${subKey}`] = null; // ドット区切りのキー
+            });
+        } else if (Array.isArray(item[key])) {
+          item[key].forEach((subItem, index) => {
+            if (typeof subItem === 'object') {
+              Object.keys(subItem).forEach(subKey => {
+                fieldArrays[`${key}.${index}.${subKey}`] = null; // ドット区切りのキー
+              });
+            } else {
+                fieldArrays[`${key}.${index}`] = null; // ドット区切りのキー
+            }
+          });
+        } else {
+            fieldArrays[key] = null; // 直接のキー
+        }
+    });
+  });
 
   // const fields = Object.keys(jsonData[0]);
-  const fields = 
-    [
-        'title',
-        'author',
-        'year',
-        'genre',
-        'chapter.title'
-    ];
+  // const fields = 
+  //   [
+  //       'title',
+  //       'author',
+  //       'year',
+  //       'genre',
+  //       'chapter.title'
+  //   ];
+  const fields = Object.keys(fieldArrays);
   console.log(fields);
   const json2csvParser = new Parser({ fields: fields, header: true , withBOM: true, flatten: true, flattenArrays: true});
   const csvData = json2csvParser.parse(jsonData);
