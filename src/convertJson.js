85a2b5599c2c5acf8505a6c3e83680541dfad159
doikota <doikota@gmail.com>
2024-09-20 00:35:23 +0900

    Refactored as recursive function

--- src/convertJson.js
+++ src/convertJson.js
@@ -5,40 +5,39 @@ function convertJson() {
   const jsonText = document.getElementById('jsonInput').value;
   const jsonData = JSON.parse(jsonText);
   console.log(jsonData);
-  const fieldArrays = {};
+  const fieldNames = {};
 
-  jsonData.forEach(item => {
-    Object.keys(item).forEach(key => {
-        if (typeof item[key] === 'object' && !Array.isArray(item[key])) {
-            Object.keys(item[key]).forEach(subKey => {
-                fieldArrays[`${key}.${subKey}`] = null; // ドット区切りのキー
-            });
-        } else if (Array.isArray(item[key])) {
-          item[key].forEach((subItem, index) => {
-            if (typeof subItem === 'object') {
-              Object.keys(subItem).forEach(subKey => {
-                fieldArrays[`${key}.${index}.${subKey}`] = null; // ドット区切りのキー
-              });
-            } else {
-                fieldArrays[`${key}.${index}`] = null; // ドット区切りのキー
-            }
-          });
-        } else {
-            fieldArrays[key] = null; // 直接のキー
-        }
+  function recursiveExtract(item, parentKey = '') {
+    Object.keys(item).forEach((key) => {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+      if (typeof item[key] === 'object' && !Array.isArray(item[key])) {
+        // オブジェクトの場合は再帰呼び出し
+        recursiveExtract(item[key], newKey);
+      } else if (Array.isArray(item[key])) {
+        // 配列の場合
+        item[key].forEach((subItem, index) => {
+          if (typeof subItem === 'object') {
+            // オブジェクトの場合は再帰呼び出し
+            recursiveExtract(subItem, `${newKey}.${index}`);
+          } else {
+            // プリミティブ値の場合
+            fieldNames[`${newKey}.${index}`] = null;
+          }
+        });
+      } else {
+        // プリミティブ値の場合
+        fieldNames[newKey] = null;
+      }
     });
-  });
+  }
 
-  // const fields = Object.keys(jsonData[0]);
-  // const fields = 
-  //   [
-  //       'title',
-  //       'author',
-  //       'year',
-  //       'genre',
-  //       'chapter.title'
-  //   ];
-  const fields = Object.keys(fieldArrays);
+jsonData.forEach(item => {
+    recursiveExtract(item);
+});
+
+
+  const fields = Object.keys(fieldNames);
   console.log(fields);
   const json2csvParser = new Parser({ fields: fields, header: true , withBOM: true, flatten: true, flattenArrays: true});
   const csvData = json2csvParser.parse(jsonData);
@@ -47,7 +46,6 @@ function convertJson() {
   // Display the converted CSV data
   const csvInput = document.getElementById('csvInput');
   csvInput.value = csvData;
-
 }
 
 window.convertJson = convertJson;
